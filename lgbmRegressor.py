c982425a1e18efb11315f933c5d93041388ba03e
Kai <kwiddeson4@gmail.com>
2022-04-13 12:07:35 +0100

    clean code

--- lgbmRegressor.py
+++ lgbmRegressor.py
@@ -1,3 +1,6 @@
+# This file contains the LightGBM and random forest predictors
+
+# imports
 import lightgbm as lgb
 import pandas as pd
 import numpy as np
@@ -8,16 +11,21 @@ import sklearn
 import random
 import math
 
+# set random seet
 random.seed(42)
 
 
+# translate from a given dictionary
 def translate(val, dic):
     return dic[val]
 
-def normalize(value, min, max):
-    return (value-min) / (max-min)
+
+# normalize value
+def normalize(value, minv, maxv):
+    return (value-minv) / (maxv-minv)
 
 
+# pre-process data by cleaning a given dataframe
 def pre_process(df, asin_convert, use_personality):
     if use_personality:
         df = df.drop(
@@ -30,14 +38,15 @@ def pre_process(df, asin_convert, use_personality):
 
     df = df.drop(columns=["reviewerID"])
 
-    # print(df.columns)
-
     df["asin"] = df.progress_apply(lambda x: translate(x.asin, asin_convert), axis=1)
 
     return df
 
 
+# create lightgbm and random forest model
 def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp, split, use_personality):
+
+    # load personality data if relevant
     if use_personality:
         if code.upper() == "K":
             personalities = pd.read_csv("Datasets/jianmoNI_UCSD_Amazon_Review_Data/2018/small/5-core/Kindle_Store_5_personality.csv")
@@ -60,45 +69,17 @@ def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp,
     R = 42
     target_col = "overall"
 
-    ######################################################
-    # # originally: p0.11 = 1.578, p0.11 = 1.530
-    # # MIGHT BE WRONG
-    # print(full_df["Extroversion"].min())
-    # print(full_df["Extroversion"].max())
-    # print(full_df)
-    # # Normalize each of the personality columns -- THIS MIGHT BE WRONG
-    # minval = full_df["Extroversion"].min()
-    # maxval = full_df["Extroversion"].max()
-    # full_df["Extroversion"] = full_df.progress_apply(lambda x: normalize(x.Extroversion, minval, maxval), axis=1)
-    # minval = full_df["Agreeableness"].min()
-    # maxval = full_df["Agreeableness"].max()
-    # full_df["Agreeableness"] = full_df.progress_apply(lambda x: normalize(x.Agreeableness, minval, maxval), axis=1)
-    # minval = full_df["conscientiousness"].min()
-    # maxval = full_df["conscientiousness"].max()
-    # full_df["conscientiousness"] = full_df.progress_apply(lambda x: normalize(x.conscientiousness, minval, maxval), axis=1)
-    # minval = full_df["Neurotisicm"].min()
-    # maxval = full_df["Neurotisicm"].max()
-    # full_df["Neurotisicm"] = full_df.progress_apply(lambda x: normalize(x.Neurotisicm, minval, maxval), axis=1)
-    # minval = full_df["Openness_to_Experience"].min()
-    # maxval = full_df["Openness_to_Experience"].max()
-    # full_df["Openness_to_Experience"] = full_df.progress_apply(lambda x: normalize(x.Openness_to_Experience, minval, maxval),
-    #                                                  axis=1)
-    # print(full_df["Extroversion"].min())
-    # print(full_df["Extroversion"].max())
-    #######################################################
-
-
+    # define user neighbourhood and target user
     neighbourhood = full_df[full_df.reviewerID != chosen_user]
     target = full_df[full_df.reviewerID == chosen_user]
 
+    # create train and target data
     train_target = target.loc[target["asin"].isin(train)]
     test_target = target.loc[target["asin"].isin(test)]
-
-    # train_target, test_target = train_test_split(target, test_size=split, random_state=R)
-
     neighbourhood = pd.concat([neighbourhood, train_target])
     target = test_target.copy()
 
+    # convert asins to numerical ids
     asins = full_df.asin.unique()
     reverse_asin_convert = {}
     id = 0
@@ -111,25 +92,24 @@ def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp,
         asin_convert[item] = id
         id += 1
 
+    # pre-process data to reduce unnecessary features
     neighbourhood = pre_process(neighbourhood, asin_convert, use_personality)
     target = pre_process(target, asin_convert, use_personality)
 
+    # create train-test split
     x_target = target.drop(columns=target_col)
     y_target = target[target_col]
-
     x = neighbourhood.drop(columns=target_col)
     y = neighbourhood[target_col]
-
     x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                           test_size=0.4,
                                                           random_state=R)
 
+    # define model to be used
     if model_choice == "L":
         model = lgb.LGBMRegressor(random_state=R)
     elif model_choice == "R":
         model = RandomForestRegressor(random_state=R)
-
-    # model.fit(x_train, y_train)
 
     # construct grid search and fit
     param_grid = create_grid(model_choice)
@@ -143,21 +123,12 @@ def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp,
     print("Best parameters: ")
     print(grid_search.best_params_)
 
-    # predictions = model.predict(x_test)
-    #
-    # print(predictions)
-    #
-    # # Generate a classification report and other metrics to determin performance
-    # print("MSE: %.3f" % sklearn.metrics.mean_squared_error(y_test, predictions))
-    # print("RMSE: %.3f" % sklearn.metrics.mean_squared_error(y_test, predictions, squared=False))
-
-    # print(x_target)
     to_predict = x_target.set_index("asin").copy()
     to_predict = to_predict[~to_predict.index.duplicated(keep='first')].reset_index()
 
-    # predictions = model.predict(x_target)
     predictions = grid_search.predict(x_target)
 
+    # output feature importance if relevant
     if disp and not grid_used:
         print()
         print("LightGBM Feature importances: ")
@@ -166,6 +137,7 @@ def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp,
             print(i, round(f * 100, 2))
         print()
 
+    # clean and sort results
     result = x_target.copy()
     result["predictions"] = predictions
     if use_personality:
@@ -175,16 +147,12 @@ def create_lightgbm(full_df, train, test, chosen_user, model_choice, code, disp,
 
     result["asin"] = result.progress_apply(lambda j: translate(j.asin, reverse_asin_convert), axis=1)
 
-    # print("result")
-    # print(result)
-
     filtered_result = result.set_index("asin").copy()
     filtered_result = filtered_result[~filtered_result.index.duplicated(keep='first')].reset_index()
 
-    print(filtered_result)
     return filtered_result
 
-
+# grid search, comments show values tested, these have been commented out to save computation time
 def create_grid(approach):
     if approach == "L":
         # num_leaves = [31, 91, 121, 151, 251, 191, 211]
@@ -208,8 +176,7 @@ def create_grid(approach):
             "learning_rate": learning_rate,
             "min_child_weight": min_child_weight,
             "min_child_samples": min_child_samples,
-            "subsample_for_bin": subsample_for_bin#,
-            # "boosting_type": boosting_type
+            "subsample_for_bin": subsample_for_bin
         }
     else:
         param_grid = {}
